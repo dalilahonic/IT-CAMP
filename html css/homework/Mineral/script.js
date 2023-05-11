4e54b5ca618b431f670b78c91688675c9a33f094
dalilahonic <dalilahonic1@gmail.com>
2023-05-11 23:05:51 +0200

    .

--- html css/homework/Mineral/script.js
+++ html css/homework/Mineral/script.js
@@ -7,10 +7,10 @@ function animateOnScroll() {
   parentContainer.forEach((par) => {
     let section = par.getBoundingClientRect().top;
     let elementAnimation = par.querySelectorAll(".elementAnimation");
-    
+
     if (section < browserPosition / 2) {
       elementAnimation.forEach((animation) => {
-        animation.classList.add("slideAnimation");
+        animation.classList.add("sliderAnimation");
       });
     }
   });
