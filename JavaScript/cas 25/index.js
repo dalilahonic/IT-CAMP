48f4956f64b15a8bb5691771f93e69a474e281d9
dalilahonic <dalilahonic1@gmail.com>
2023-07-14 17:01:45 +0200

    .

--- JavaScript/cas 25/index.js
+++ JavaScript/cas 25/index.js
@@ -28,25 +28,29 @@ class Prodavnica {
   pretraziProizvode(minCena, maxCena) {
     let ceneProizvoda = [];
 
-    for(let obj of this.proizvodi) {
-        for(let [key, value] of Object.entries(obj)) {
-            if(key === 'cena' && value > minCena && value < maxCena ) {
-                ceneProizvoda.push(obj);
-            } 
+    for (let obj of this.proizvodi) {
+      for (let [key, value] of Object.entries(obj)) {
+        if (
+          key === 'cena' &&
+          value >= minCena &&
+          value < maxCena
+        ) {
+          ceneProizvoda.push(obj);
         }
+      }
     }
     return ceneProizvoda;
   }
 
   ukupnaVrednostProizvoda() {
     let sum = 0;
-    for(let obj of this.proizvodi) {
-        let kolicina = obj.kolicina;
-        let cena = obj.cena;
-        sum += (cena * kolicina);
+    for (let obj of this.proizvodi) {
+      let kolicina = obj.kolicina;
+      let cena = obj.cena;
+      sum += cena * kolicina;
     }
-    return sum
-  } 
+    return sum;
+  }
 }
 
 // let arr = [1,2,3,4,5,6];
@@ -66,7 +70,7 @@ prodavnica1.ukloniProizvod('hleb');
 console.log(prodavnica1);
 
 prodavnica1.dodajProizvod('hleb', 70, 3);
-prodavnica1.dodajProizvod('taj' , 670, 10);
+prodavnica1.dodajProizvod('taj', 670, 10);
 // console.log(prodavnica1);
 
 console.log(prodavnica1.pretraziProizvode(60, 210));
