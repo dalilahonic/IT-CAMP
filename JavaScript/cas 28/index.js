f53a32cb88c4eeb5286bcd8da744ff2dd3802d59
dalilahonic <dalilahonic1@gmail.com>
2023-07-24 23:22:22 +0200

    .

---
+++ JavaScript/cas 28/index.js
@@ -0,0 +1,17 @@
+// setTimeout(()=> {
+//     console.log('ovo');
+// },0)
+
+// const test = new Promise((res, rej) => {
+//     res()
+// })
+
+// test.then((res) => {
+//     for (let i = 0; i < 1000000044; i++) {
+//         if(i === 1_000_000_043) console.log('ovo je ' + i);
+        
+//     }
+// })
+
+// console.log('sdnfjnnfnfnfn');
+
